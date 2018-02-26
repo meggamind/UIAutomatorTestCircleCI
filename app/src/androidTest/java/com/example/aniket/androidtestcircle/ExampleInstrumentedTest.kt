51aade0f01515f9f7a21623958555c4777170d31
meggamind <aps431@nyu.edu>
2018-02-26 14:15:18 -0800

    first commit

---
+++ app/src/androidTest/java/com/example/aniket/androidtestcircle/ExampleInstrumentedTest.kt
@@ -0,0 +1,49 @@
+package com.example.aniket.androidtestcircle
+
+import android.content.Intent
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+import android.support.test.uiautomator.*
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+import org.junit.Before
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    private var mDevice: UiDevice? = null
+
+    @Before
+    fun initITSetup() {
+        mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testLoginProcess() {
+        val context = InstrumentationRegistry.getContext()
+        val intent = context.packageManager.getLaunchIntentForPackage(APP_NAME)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        context.startActivity(intent)
+        println("mDevice!!.currentPackageName: " + mDevice!!.currentPackageName)
+
+        mDevice!!.wait(Until.hasObject(By.textContains("TESTING BUTTON")), 60000 * 1)
+        if (!UiObject(UiSelector().textContains("TESTING BUTTON")).exists()) {
+            println("View not Found ")
+            throw Exception("View not Found")
+        } else {
+            println("Found View")
+        }
+    }
+
+    companion object {
+        const val APP_NAME = "com.example.aniket.androidtestcircle"
+    }
+}
